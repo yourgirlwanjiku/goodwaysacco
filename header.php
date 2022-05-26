5fcff8780fcddda241e04c0df5e607ede2fae6e7
yourgirlwanjiku <55155480+yourgirlwanjiku@users.noreply.github.com>
2022-05-26 14:34:53 +0300

    Add files via upload

---
+++ header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- Main CSS -->
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="boxicons-2.0.9/css/boxicons.min.css">
+    <title>GOODWAY | SACCO</title>
+</head>
+
+    <body>
+        <div class="showcase" >
+            
+             <!-- navbar top section -->
+            <div class="navbar-top">
+                <ul class="left">
+                    <li><a href="#">GOODWAY</a></li>
+                    <li><a href="#"><i class="bx bxs-time bx-xs"></i> Mon - Fri: 8:00 - 4:00</a></li>
+                    <li><a href="#"><i class="bx bxs-time bx-xs"></i> Sat: 8:30 - 12:30</a></li>
+                    <li><a href="#"><i class="bx bxs-map bx-xs"></i> 411 University St. Seatle</a></li>
+                    <li><a href="#"><i class="bx bxs-phone bx-xs"></i> +1-700-896-002-34</a></li>
+                </ul>
+                <ul class="right">
+                    <li><a href="#"><i class="bx bxl-twitter bx-xs"></i></a></li>
+                    <li><a href="#"><i class="bx bxl-pinterest-alt bx-xs"></i></a></li>
+                    <li><a href="#"><i class="bx bxl-facebook bx-xs"></i></a></li>
+                    <li><a href="#"><i class="bx bxl-instagram-alt bx-xs"></i></a></li>
+                </ul>
+            </div>
+             <!-- navbar bottom section -->
+       <div class="navbar-bottom">
+        <a href="#home" class="brand-left">GOODWAY</a>
+        <ul class="menu-right">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#services">Services</a></li>
+            <li><a href="#portfolio">About Us</a></li>
+            <li><a href="#teams">Teams</a></li>
+            <li><a href="#blog">Blog</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="#"><i class="bx bx-search bx-sm" id="search-btn"></i></a></li>
+        </ul>
+        <div class="search-form">
+            <input type="search" name="search-box" id="search-box" placeholder="search here...">
+            <label for="search-box" class="bx bx-search bx-sm"></label>
+        </div>
+   </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,256 @@
+
+    <?php
+include "header.php";
+?>
+     <!--cotent section -->
+       <div class="showcase-content">
+           <h1>Growing <br>  together  <span> Economically  </span>  </h1>
+           <p> Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+           <a href="#" class=" btn btn-left"> ABOUT US</a>
+           <a href="#" class=" btn btn-right"> ABOUT US</a>
+       </div>
+       <!--SErvices/goal section-->
+       <section id="services" class="bg-light py-3">
+           <div class="wrapper">
+               <div class="items">
+                   <div class="item">
+                    <i class="bx bxs-badge-dollar bx-lg"></i>
+                    <h3>Investment</h3>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, blanditiis.</p>
+                        <a href="#" class=" btn "> Learn More</a><!--work on the button-->
+                   </div>
+                   <div class="item">
+                    <i class="bx bxs-credit-card-front bx-lg"></i>
+                    <h3>Loans</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, blanditiis.</p>
+                    <a href="#" class=" btn "> Learn More</a>
+                </div>
+                <div class="item">
+                <i class="bx bxs-check-shield bx-lg"></i><!--to look for a simillar icon-->
+                    <h3>Policies</h3>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, blanditiis.</p>
+                </div>
+            </div>
+            <div id="goal" class="goal">
+                <div class="goal-img">
+                    <img src="./images/background.jpg">
+               </div>
+               <div class="goal-text text-center">
+                <a href="#">ABOUT US</a>
+                
+                <div class="contents">
+                    <div class="content p-4">
+                      
+                    </div>
+                    <div class="content p-4">
+                        <h3>HISTORY</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Nam ipsa vero accusantium enim culpa iure aspernatur ?</p>
+                    </div>
+                </div>
+                <div class="contents">
+                    <div class="content p-4">
+                        
+                    </div>
+                    <div class="content p-4">
+                        <h3>MISSION</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Nam ipsa vero accusantium enim culpa iure aspernatur ?</p>
+                    </div>
+                </div>
+                <div class="contents">
+                    <div class="content p-4">
+                       
+                    </div>
+                    <div class="content p-4">
+                        <h3>VISION</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Nam ipsa vero accusantium enim culpa iure aspernatur ?</p>
+                    </div>
+                </div>
+               
+               </div>
+
+            </div>
+           </div>
+
+       </section>
+
+         <!-- Team Section -->
+    <section id="teams" class="teams bg-light py-3">
+        <h3 class="text-center">We are there for you</h3>
+        <h2 class="text-center">Meet Our <span class="text-secondary">BOARD MEMBERS</span></h2>
+        <p class="text-center">We help you see the world differently, discover opportunities you may never have <br> imagined and achieve results that bridge what is with what can be</p>
+        <div class="wrapper">
+            <div class="card-items">
+                <div class="card">
+                    <div class="card-header">
+                        <img src="/images/cynthia.jpg">
+                    </div>
+                    <div class="card-body">
+                        <h3>Cynthia Spears</h3>
+                        <h4>Chief Communication Officer</h4>
+                        <ul>
+                            <li><a href="#"><i class="bx bxl-twitter bx-xs"></i></a></li>
+                            <li><a href="#"><i class="bx bxl-pinterest-alt bx-xs"></i></a></li>
+                            <li><a href="#"><i class="bx bxl-facebook bx-xs"></i></a></li>
+                            <li><a href="#"><i class="bx bxl-instagram-alt bx-xs"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <img src="/images/vera.jpg">
+                    </div>
+                    <div class="card-body">
+                        <h3>Vera Monica</h3>
+                        <h4>Chief Business Project</h4>
+                        <ul>
+                            <li><a href="#"><i class="bx bxl-twitter bx-xs"></i></a></li>
+                            <li><a href="#"><i class="bx bxl-pinterest-alt bx-xs"></i></a></li>
+                            <li><a href="#"><i class="bx bxl-facebook bx-xs"></i></a></li>
+                            <li><a href="#"><i class="bx bxl-instagram-alt bx-xs"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <img src="/images/kevin.jpg">
+                    </div>
+                    <div class="card-body">
+                        <h3>Kevin Philips</h3>
+                        <h4>International Consultant</h4>
+                        <ul>
+                            <li><a href="#"><i class="bx bxl-twitter bx-xs"></i></a></li>
+                            <li><a href="#"><i class="bx bxl-pinterest-alt bx-xs"></i></a></li>
+                            <li><a href="#"><i class="bx bxl-facebook bx-xs"></i></a></li>
+                            <li><a href="#"><i class="bx bxl-instagram-alt bx-xs"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div class="card">
+                    <div class="card-header">
+                        <img src="/images/augusta.jpg">
+                    </div>
+                    <div class="card-body">
+                        <h3>Augusta Brian</h3>
+                        <h4>Co-Founder of Company</h4>
+                        <ul>
+                            <li><a href="#"><i class="bx bxl-twitter bx-xs"></i></a></li>
+                            <li><a href="#"><i class="bx bxl-pinterest-alt bx-xs"></i></a></li>
+                            <li><a href="#"><i class="bx bxl-facebook bx-xs"></i></a></li>
+                            <li><a href="#"><i class="bx bxl-instagram-alt bx-xs"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Blog Section -->
+    <section id="blog" class="blog py-3">
+        <div class="wrapper">
+            <div class="header">
+                <span></span>
+                <h4>Business Blog</h4>
+            </div>
+            <div class="blog-info">
+                <h2>Read Our <span class="text-secondary">Latest Insights</span></h2>
+                <a href="#">view all</a>
+            </div>
+            <div class="blog-card">
+                <div class="card">
+                    <div class="card-header">
+                        <img src="/images/blog1.jpg">
+                    </div>
+                    <div class="card-body">
+                        <span class="tag tag-yellow">Business</span>
+                        <h4>Contactless Payments' <br> Time has come</h4>
+                        <div class="footer">
+                            <small>April 7, 2022 / 5 comments</small>
+                            <a href="#"><i class="bx bx-right-arrow-alt bx-sm"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <img src="/images/blog2.jpg">
+                    </div>
+                    <div class="card-body">
+                        <span class="tag tag-purple">Finance</span>
+                        <h4>The Future of Retail: <br> Europe's Ecosystems</h4>
+                        <div class="footer">
+                            <small>April 11, 2022 / 20 comments</small>
+                            <a href="#"><i class="bx bx-right-arrow-alt bx-sm"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-header">
+                        <img src="/images/blog3.jpg">
+                    </div>
+                    <div class="card-body">
+                        <span class="tag tag-pink">Marketiing</span>
+                        <h4>Digital Transformation <br> Roadmap: Enablers</h4>
+                        <div class="footer">
+                            <small>April 30, 2022 / 10 comments</small>
+                            <a href="#"><i class="bx bx-right-arrow-alt bx-sm"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+  
+
+       <!--footer-->
+
+       <footer class="footer">
+          <div class="footer-left">
+            <img src="./images/logo.jpg" alt="">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quibusdam ullam eos tenetur </p>
+         <div class="socials">
+            <a href="#"><i class="bx bxl-twitter bx-xs"></i></a>
+            <a href="#"><i class="bx bxl-pinterest-alt bx-xs"></i></a>
+            <a href="#"><i class="bx bxl-facebook bx-xs"></i></a>
+            <a href="#"><i class="bx bxl-instagram-alt bx-xs"></i></a>
+         </div>
+            </div> 
+        <ul class="footer-right">
+            <li >
+                <h2>Product</h2>
+                <ul>
+                    <li> <a href="#"></a>theme design</li>
+                    <li> <a href="#"></a>theme design</li>
+                    <li> <a href="#"></a>theme design</li>
+                </ul>
+            </li>
+            <li class="features" >
+                <h2>Product</h2>
+                <ul class="box">
+                    <li> <a href="#"></a>theme design</li>
+                    <li> <a href="#"></a>theme design</li>
+                    <li> <a href="#"></a>theme design</li>
+                </ul>
+            </li>
+            <li >
+                <h2>Product</h2>
+                <ul class="box">
+                    <li> <a href="#"></a>theme design</li>
+                    <li> <a href="#"></a>theme design</li>
+                    <li> <a href="#"></a>theme design</li>
+                </ul>
+            </li>
+        </ul>
+        
+        
+        <div class="footer-bottom">
+            <p>All Rights Reserverd &copy; Goodway Sacco 2022</p>
+        </div>
+       </footer>
+          
+        
+        <!--scripts-->
+        <script src="js/goodway.js"></script>
+
+    </body>
+    </html>
